1461f586d3546e6eb8d2796985d5053d7c214c07
Alex Onufrak <60350877+alexonufrak@users.noreply.github.com>
2025-03-21 15:26:53 -0400

    Update DashboardContext.js

--- contexts/DashboardContext.js
+++ contexts/DashboardContext.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { createContext, useContext, useState, useMemo } from "react"
+import { createContext, useContext, useState, useMemo, useEffect } from "react"
 import { useUser } from "@auth0/nextjs-auth0"
 import { useQueryClient } from '@tanstack/react-query'
 import { 
